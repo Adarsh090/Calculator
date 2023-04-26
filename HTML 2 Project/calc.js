7baf1ce37481d329f84589cac2b4c062b4a44f88
Adarsh090 <89680062+Adarsh090@users.noreply.github.com>
2023-04-26 20:27:06 +0530

    Add files via upload

---
+++ HTML 2 Project/calc.js
@@ -0,0 +1,97 @@
+var numkeys = [];
+var opns = [];
+var res = document.querySelector("#resultArea");
+var clrBtn = document.querySelector("#clrTxt");
+var delBtn = document.querySelector("#del");
+var eqBtn = document.querySelector("#eq");
+var decPoint = document.querySelector("#decp");
+
+var opnSyms = ["+", "-", "*", "/"];
+var symList;
+var tmpCal;
+var j;
+var duplicate;
+var r = false;
+
+
+for (var i = 0; i <= 9; i++) {
+    (function (i){
+        qs = "#num" + i;
+        numkeys.push(document.querySelector(qs));
+        
+        numkeys[i].addEventListener("click", function () {
+            if ( r){
+                res.textContent = i;
+                r = false;
+                return;
+            }
+            res.textContent += i;
+
+        });
+    })(i);
+}
+
+for (var i = 0; i <= 3; i++) {
+    (function (i) {
+        qs = "#op" + i;
+        opns.push(document.querySelector(qs));
+        opns[i].addEventListener("click", function () {
+            console.log(i)
+            res.textContent += opnSyms[i];
+        });
+    }(i));
+}
+
+clrBtn.addEventListener("click", function(){
+    res.textContent = "";
+
+});
+
+delBtn.addEventListener("click", function(){
+//     temp=res.textContent[res.textContent.length-1];
+//     res.textContent = res.textContent.substring(0, res.textContent.length -1);
+//     if((temp!="+") && temp!="-" && temp!= "*" && temp!="/"){
+//         duplicate=res.textContent.substring(0,res.textContent.length -1);
+//     if(res.textContent[res.textContent.length-1]!="+"&& res.textContent[res.textContent.length-1]!="-") res.textContent[
+    
+//     }
+//     else{
+//         duplicate=res.textContent.substring(0,res.textContent.length -1);
+//     }
+
+// }
+// res.textContent = "";
+
+    if(res.textContent.length != 0){
+        res.textContent = res.textContent.substring(0, res.textContent.length -1);
+        return;
+    }
+
+})
+
+
+
+decPoint.addEventListener("click", function(){
+    res.textContent += ".";
+});
+
+eqBtn.addEventListener("click", function(){
+    (function(i,j){
+        symList =[];
+        for(var i=0; i<opnSyms.length;i++){
+            if(res.textContent.indexOf(opnSyms[i]) > -1){
+                symList.push(opnSyms[i]);
+            }
+        }
+
+        if(symList.length > 0){
+            try{
+                res.textContent = eval(res.textContent);
+            }
+            catch(e){
+                res.textContent = "Invalid Syntax";
+            }
+            r = true;
+        }
+    }(i,j));
+});
